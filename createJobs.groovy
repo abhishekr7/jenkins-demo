ecec01d98dfbccf2d9ca2c06cd50600c9fe09507
abhishekr7 <abhishekrajbhoj38@gmail.com>
2021-07-21 19:13:06 +0530

    Create Job for jenkins demo

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,14 @@
-pipelineJob('pipelineJob') {
+pipelineJob('jenkins-demo-sample-job') {
     definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/abhishekr7/demo.git'
+                    }
+                    branch 'master'
+                }
+            }
         }
     }
 }
